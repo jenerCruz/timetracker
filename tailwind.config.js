328a2cada3179c53a36fe9b5478ee5738a84937d
jenerCruz <pamecoaxaca.telcdl@gmail.com>
2025-11-26 13:41:35 -0600

    otro

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./assets/js/**/*.js",
+    "./components/**/*.html",
+    "./components/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
